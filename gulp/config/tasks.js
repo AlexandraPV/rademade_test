ca3b2f78c66a2dafba35b492e0f96487dfa4c2fa
Alexandra Pyvovarchuk <alisandra.pv@gmail.com>
2019-11-12 19:54:01 +0200

    test

---
+++ gulp/config/tasks.js
@@ -0,0 +1,9 @@
+module.exports = [
+    './gulp/tasks/pug',
+    './gulp/tasks/serve',
+    './gulp/tasks/sass',
+    './gulp/tasks/watch',
+    './gulp/tasks/img',
+    './gulp/tasks/fonts',
+    './gulp/tasks/svg'
+];
